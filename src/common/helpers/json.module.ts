48ef6c03fd08a5461ab002f86fa186ef3bcde40f
v1pi <vimivini99@gmail.com>
2020-09-23 20:33:02 -0300

    Modificado nome do teste e deletado modulo não utilizado

--- src/common/helpers/json.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common'
-import { JsonHelper } from './json.helper'
-
-@Module({
-  exports: [JsonHelper],
-})
-export class JsonHelperModule {}
--- src/prestadores/prestadores.service.spec.ts
+++ src/prestadores/prestadores.service.spec.ts
@@ -1,18 +1,40 @@
 import { Test, TestingModule } from '@nestjs/testing'
+import { PrestadoresController } from './prestadores.controller'
 import { PrestadoresService } from './prestadores.service'
+import { PrestadoresEntityModule } from '../models/prestadores/prestadores.module'
+import { createMock } from '@golevelup/nestjs-testing'
+import { Prestador } from '../models/prestadores/prestador.entity'
+import { Repository } from 'typeorm'
+import { getRepositoryToken } from '@nestjs/typeorm'
 
-describe('PrestadoresService', () => {
+describe('PrestadoresController', () => {
   let service: PrestadoresService
+  const repo = createMock<Repository<Prestador>>()
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PrestadoresService],
+      providers: [
+        PrestadoresService,
+        {
+          provide: getRepositoryToken(Prestador),
+          useValue: repo,
+        },
+      ],
     }).compile()
 
     service = module.get<PrestadoresService>(PrestadoresService)
   })
 
-  it('should be defined', () => {
+  it('should be defined', async () => {
+    const shouldReturn = [
+      {
+        nome: 'Vinicius Picanco',
+      },
+    ]
     expect(service).toBeDefined()
+
+    repo.find.mockReturnValue(shouldReturn as any)
+
+    expect(await service.getAll()).toBe(shouldReturn)
   })
 })
