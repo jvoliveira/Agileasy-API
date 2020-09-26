fbbb60e76e93110df7476dcef7c5f9720d3826b1
v1pi <vimivini99@gmail.com>
2020-09-26 11:49:49 -0300

    Lidando com exceptions http

--- src/common/exceptions/all-exceptions.filter.ts
+++ src/common/exceptions/all-exceptions.filter.ts
@@ -18,7 +18,6 @@ export class AllExceptionsFilter implements ExceptionFilter {
       exception instanceof HttpException
         ? exception.getStatus()
         : HttpStatus.INTERNAL_SERVER_ERROR
-
     if (exception instanceof AllException) {
       response.status(status).json({
         error: true,
@@ -27,11 +26,33 @@ export class AllExceptionsFilter implements ExceptionFilter {
         message: exception.tipoErroDado.message,
       })
     } else {
+      // Define a mensagem padrão ser erro desconhecido
+      let message = 'Erro desconhecido'
+      let error_id = TipoErro.ERROR_DESCONHECIDO
+      // Caso a exceção seja uma exceção HTTP genérica
+      if (exception instanceof HttpException) {
+        // Pega a resposta
+        const response = exception.getResponse()
+        error_id = exception.getStatus()
+        // Caso seja um objeto acessa o seu atributo message
+        if (response instanceof Object) {
+          const msg = response['message']
+          // Se for um array pega a primeira posição, caso não pega a string retornada no objeto message
+          if (msg instanceof Array) {
+            message = response['message'][0]
+          } else {
+            message = response['message']
+          }
+        } else {
+          // Pega simplesmente a resposta que também pode ser uma string
+          message = response
+        }
+      }
       response.status(status).json({
         error: true,
-        error_id: TipoErro.ERROR_DESCONHECIDO,
+        error_id,
         data: [],
-        message: 'Erro desconhecido',
+        message,
       })
     }
   }
