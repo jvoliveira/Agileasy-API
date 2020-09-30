82ef5a6855d192e40986e423da956adb21bcbce0
v1pi <vimivini99@gmail.com>
2020-09-30 19:21:01 -0300

    Modificado nome das variáveis de ambiente

--- src/config/database/postgresql/configuration.ts
+++ src/config/database/postgresql/configuration.ts
@@ -1,7 +1,8 @@
 import { registerAs } from '@nestjs/config'
 export default registerAs('pg-models', () => ({
-  databaseName: process.env.NAME_BD,
-  host: process.env.HOST_BD,
-  port: process.env.PORT_BD,
-  password: process.env.PASSWORD_BD,
+  databaseName: process.env.POSTGRES_DB,
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  password: process.env.POSTGRES_PASSWORD,
+  user: process.env.POSTGRES_USER,
 }))
