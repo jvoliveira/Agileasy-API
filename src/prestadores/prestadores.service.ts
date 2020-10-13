bdc4433bc806db53d7542eaeaba9b145d5c38f09
v1pi <vimivini99@gmail.com>
2020-10-13 09:17:23 -0300

    Removido dependência não utilizada

--- src/prestadores/prestadores.service.ts
+++ src/prestadores/prestadores.service.ts
@@ -3,7 +3,6 @@ import { Prestador } from '../models/prestadores/prestador.entity'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
 import { BaseService } from '../common/services/base.service'
-import { Categoria } from '../models/categorias/categoria.entity'
 import { AllException } from '../common/exceptions/all.exception'
 import { TipoErro } from '../common/enums/tipo-erro.enum'
 
