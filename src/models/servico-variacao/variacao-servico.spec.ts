6be84e634e1b3fad195f8453a8379885e911e7a5
v1pi <vimivini99@gmail.com>
2020-11-04 16:46:39 -0300

    Corrigido testes

--- src/models/servico-variacao/variacao-servico.spec.ts
+++ src/models/servico-variacao/variacao-servico.spec.ts
@@ -2,14 +2,7 @@ import { VariacaoServico } from './variacao-servico.entity'
 
 describe('Variacao-Servico', () => {
   it('test json Variacao-Servico', () => {
-    const mockVariacao = new VariacaoServico(
-      1,
-      1,
-      'Escolha o modelo',
-      true,
-      1,
-      1,
-    )
+    const mockVariacao = new VariacaoServico(1, 1, 'Escolha o modelo', true, 1)
     mockVariacao.alternativas = []
     expect(JSON.parse(JSON.stringify(mockVariacao))).toStrictEqual(
       JSON.parse(JSON.stringify(expectedJSON())),
@@ -26,7 +19,6 @@ function expectedJSON() {
     tipo: 1,
     titulo: 'Escolha o modelo',
     obrigatorio: true,
-    id_servico: 1,
     qtsMaxima: 1,
   }
 }
