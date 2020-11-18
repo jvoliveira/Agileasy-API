ed4097cfe86116cb0bc09e237b260a37078d1001
v1pi <vimivini99@gmail.com>
2020-11-17 21:27:35 -0300

    Corrigido modelo disponibilidade

--- src/models/disponibilidades/disponibilidade.interface.ts
+++ src/models/disponibilidades/disponibilidade.interface.ts
@@ -1,3 +1,14 @@
+export enum DiaSemana {
+  QUALQUER = -1,
+  SEGUNDA = 0,
+  TERCA = 1,
+  QUARTA = 2,
+  QUINTA = 3,
+  SEXTA = 4,
+  SABADO = 5,
+  DOMINGO = 7,
+}
+
 export interface DisponibilidadeInterface {
   diaSemana: number
   inicio: Date
--- src/models/disponibilidades/disponibilidade.module.ts
+++ src/models/disponibilidades/disponibilidade.module.ts
@@ -1,6 +1,5 @@
 import { Module, forwardRef } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
-import { PedidosEntityModule } from '../pedidos/pedidos.module'
 import { Disponibilidade } from './disponibilidade.entity'
 import { PrestadoresEntityModule } from '../prestadores/prestadores.module'
 
@@ -8,7 +7,6 @@ import { PrestadoresEntityModule } from '../prestadores/prestadores.module'
   imports: [
     TypeOrmModule.forFeature([Disponibilidade]),
     forwardRef(() => PrestadoresEntityModule),
-    forwardRef(() => PedidosEntityModule),
   ],
   exports: [TypeOrmModule],
 })
