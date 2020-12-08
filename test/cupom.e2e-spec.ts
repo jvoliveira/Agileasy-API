6a5eae73466984de8d673d278249e2a042cbd50d
v1pi <vimivini99@gmail.com>
2020-12-08 15:05:30 -0300

    Adicionado testes para checar cupom

--- test/cupom.e2e-spec.ts
+++ test/cupom.e2e-spec.ts
@@ -3,15 +3,19 @@ import { INestApplication } from '@nestjs/common'
 import * as request from 'supertest'
 import { createMock } from '@golevelup/nestjs-testing'
 import { AppModule } from '../src/app.module'
-import { Repository } from 'typeorm'
+import { Repository, SelectQueryBuilder } from 'typeorm'
 import { Cupom } from '../src/models/cupons/cupom.entity'
 import { getRepositoryToken } from '@nestjs/typeorm'
 import { FirebaseAuthenticationService } from '@aginix/nestjs-firebase-admin'
 import { TipoUsuario } from '../src/common/enums/tipo-usuario.enum'
+import { Pedido } from '../src/models/pedidos/pedido.entity'
+import { Usuario } from '../src/models/usuarios/usuario.entity'
 
 describe('CuponsController (e2e)', () => {
   let app: INestApplication
   const mockRepo = createMock<Repository<Cupom>>()
+  const mockPedido = createMock<Repository<Pedido>>()
+  const mockUsuario = createMock<Repository<Usuario>>()
   const mockFirebaseAuth = createMock<FirebaseAuthenticationService>()
 
   beforeAll(async () => {
@@ -23,6 +27,10 @@ describe('CuponsController (e2e)', () => {
       .useValue(mockRepo)
       .overrideProvider(FirebaseAuthenticationService)
       .useValue(mockFirebaseAuth)
+      .overrideProvider(getRepositoryToken(Pedido))
+      .useValue(mockPedido)
+      .overrideProvider(getRepositoryToken(Usuario))
+      .useValue(mockUsuario)
       .compile()
     app = moduleFixture.createNestApplication()
     app.init()
@@ -53,6 +61,8 @@ describe('CuponsController (e2e)', () => {
           tipoDesconto: 1,
           validade: '2020-12-29T03:00:00Z',
           ativo: true,
+          restantes: 1,
+          quantidadeMaxima: 1,
         },
       },
     }
@@ -78,9 +88,64 @@ describe('CuponsController (e2e)', () => {
         tipoDesconto: 1,
         validade: '2020-12-29T00:00:00-03:00',
         ativo: true,
+        restantes: 1,
+        quantidadeMaxima: 1,
       })
       .auth('token-valido', { type: 'bearer' })
     expect(response.status).toBe(201)
+    expect(response.body).toStrictEqual(shouldReturn)
+  })
+
+  it('/cupons/:codigo/validar/cliente/eu (PATCH)', async () => {
+    const shouldReturn = {
+      error_id: -1,
+      message: 'Sucesso!',
+      error: false,
+      data: {
+        cupom: {
+          id: 1,
+          ativo: true,
+          indicacao: false,
+          codigo: '#ITAPERUNA',
+          desconto: 10,
+          valorMinimo: 2,
+          tipoCupom: 0,
+          tipoDesconto: 1,
+          voucher: 0,
+          validade: '2100-12-29T05:00:00.000Z',
+          quantidadeMaxima: 1,
+          restantes: 1,
+        },
+      },
+    }
+
+    mockRepo.find.mockResolvedValue([shouldReturn.data.cupom] as any)
+    mockUsuario.findOne.mockResolvedValue({ cliente: { id: 1 } } as any)
+
+    const mockQueryBuilder = createMock<SelectQueryBuilder<Pedido>>()
+    const mockLeftJoinAndSelect1 = createMock<SelectQueryBuilder<Pedido>>()
+    const mockLeftJoinAndSelect2 = createMock<SelectQueryBuilder<Pedido>>()
+    const mockWhere = createMock<SelectQueryBuilder<Pedido>>()
+
+    mockPedido.createQueryBuilder.mockReturnValue(mockQueryBuilder)
+    mockQueryBuilder.leftJoinAndSelect.mockReturnValue(mockLeftJoinAndSelect1)
+    mockLeftJoinAndSelect1.leftJoinAndSelect.mockReturnValue(
+      mockLeftJoinAndSelect2,
+    )
+    mockLeftJoinAndSelect2.where.mockReturnValue(mockWhere)
+    mockWhere.getCount.mockResolvedValue(0)
+
+    mockFirebaseAuth.verifyIdToken.mockResolvedValue({
+      uid: 'uid-valido',
+    } as any)
+    mockFirebaseAuth.getUser.mockResolvedValue({
+      customClaims: { roles: [TipoUsuario.CLIENTE] },
+    } as any)
+
+    const response = await request(app.getHttpServer())
+      .patch('/cupons/%23ITAPERUNA/validar/cliente/eu')
+      .auth('token-valido', { type: 'bearer' })
+    expect(response.status).toBe(200)
     expect(response.body).toStrictEqual(shouldReturn)
   })
 })
