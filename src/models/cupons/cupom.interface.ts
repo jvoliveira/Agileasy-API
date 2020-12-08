ea47eaa55adc039cc895ff44e56fd567c8f7237b
v1pi <vimivini99@gmail.com>
2020-12-08 13:06:58 -0300

    Adicionado enums

--- src/models/cupons/cupom.interface.ts
+++ src/models/cupons/cupom.interface.ts
@@ -1,3 +1,15 @@
+export enum TipoDesconto {
+  VALOR = 0,
+  PORCENTAGEM = 1,
+}
+
+export enum TipoCupom {
+  NORMAL = 0, // Pode ser usado somente uma vez por usuário
+  INDICACAO = 1, // Somente no primeiro serviço
+  POR_CLIENTE = 2, // Cupom que o cliente recebe sem pedir, válido somente para alguns clientes
+  POR_PRESTADOR = 3, // Cupom válidos somente para alguns prestadores
+}
+
 export interface CupomInterface {
   codigo: string
   desconto: number
