7270498d04f857a8252313955ad496ce6c329aab
v1pi <vimivini99@gmail.com>
2020-12-08 13:04:22 -0300

    Colocado para false cascade

--- src/models/cupons/cupom.entity.ts
+++ src/models/cupons/cupom.entity.ts
@@ -86,7 +86,7 @@ export class Cupom extends BaseModel<Cupom> implements CupomInterface {
   @OneToMany(
     type => Pedido,
     pedidos => pedidos.cupom,
-    { cascade: true },
+    { cascade: false },
   )
   pedidos!: Pedido[]
 
