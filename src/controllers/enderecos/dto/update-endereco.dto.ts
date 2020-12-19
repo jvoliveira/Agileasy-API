192519ed59ed379b33f801bb4b708742f8f9e102
v1pi <vimivini99@gmail.com>
2020-12-19 12:15:56 -0300

    Retirado obrigatoriedade de ser numero

--- src/controllers/enderecos/dto/update-endereco.dto.ts
+++ src/controllers/enderecos/dto/update-endereco.dto.ts
@@ -1,4 +1,4 @@
-import { IsNumberString, IsString } from 'class-validator'
+import { IsString } from 'class-validator'
 
 export class UpdateEnderecoDto {
   @IsString()
@@ -6,7 +6,7 @@ export class UpdateEnderecoDto {
   @IsString()
   endereco: string
   complemento: string | null
-  @IsNumberString()
+  @IsString()
   numero: string
   @IsString()
   cidade: string
