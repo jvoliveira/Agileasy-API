b106e6ea3fe676e17ff9418a055ae3394d4987a4
v1pi <vimivini99@gmail.com>
2020-12-21 16:16:46 -0300

    Corrigido modulo

--- src/models/folha-resposta/folha-reposta.module.ts
+++ src/models/folha-resposta/folha-reposta.module.ts
@@ -2,10 +2,11 @@ import { Module, forwardRef } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { PedidosEntityModule } from '../pedidos/pedidos.module'
 import { AlternativaEntityModule } from '../alternativa/alternativa.module'
+import { FolhaResposta } from './folha-resposta.entity'
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([]),
+    TypeOrmModule.forFeature([FolhaResposta]),
     forwardRef(() => AlternativaEntityModule),
     forwardRef(() => PedidosEntityModule),
   ],
