00ed072b83f9e68aa460b9d52e5bde89ece3439c
v1pi <vimivini99@gmail.com>
2020-12-22 09:53:02 -0300

    Removido dependências inúteis

--- src/controllers/avaliacoes/avaliacoes.controller.ts
+++ src/controllers/avaliacoes/avaliacoes.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Param, Post } from '@nestjs/common'
+import { Body, Controller, Post } from '@nestjs/common'
 import { Roles } from '../../common/decorators/roles.decorator'
 import { User } from '../../common/decorators/user.decorator'
 import { TipoUsuario } from '../../common/enums/tipo-usuario.enum'
